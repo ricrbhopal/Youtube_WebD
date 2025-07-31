02e16405eda51870f0a1130fe6506ffea2f0a0ae
Raj Vardhan <rajvardhan@ricr.in>
2025-07-31 19:18:18 +0530

    Recoding Day 1

---
+++ JavaScript/OperatorsAndExpression/script.js
@@ -0,0 +1,44 @@
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+
+let x = 5;
+let y = 2;
+
+console.log(x / y);
+console.log(x % y);
+
+a++;
+console.log(a);
+
+a *= 5;
+
+console.log(a);
+
+let p = 5;
+
+let q = "5";
+
+
+console.log(p==q);
+
+
+console.log(p===q);
+
+
+let m = true;
+let n = false;
+
+console.log(m&&n);
+
+console.log(m||n);
+
+
+
+console.log(typeof m);
+
+
+console.log(typeof p)
+console.log(typeof q)
+
---
+++ JavaScript/variablesAndDataTypes/script.js
@@ -0,0 +1,22 @@
+var x=5;
+var x=10;
+
+x=15;
+
+
+
+let y =10;
+y=11;
+
+let a = 20;
+a=25;
+
+const z = 20;
+
+
+
+console.log(x);
+
+console.log(y);
+
+console.log(z);
